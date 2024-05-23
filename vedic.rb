995b9c03efd68d2b8524528800879f493d16f16b
Sumeet Singh <sumeet.singhji@outlook.com>
2024-05-23 22:45:42 +1000

    v0.1

--- vedic.rb
+++ vedic.rb
@@ -4,33 +4,24 @@ class Vedic < Formula
   version "2.0.6"
   url "https://github.com/vedic-lang/vedic/archive/refs/tags/v#{version}.tar.gz"
   sha256 "5453386fcebfa48a5f3d39bc832d19edaaaa98d8d36841428ff6a20db0dd0151"
-  
+
   livecheck do
     url :stable
     regex(/release[._-](\d+(?:\.\d+)+)/i)
     strategy :github_latest
   end
 
+  depends_on "rust" => :build
+
   def install
-    on_macos do
-      on_arm do
-        url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-darwin-aarch64.tar.gz"
-        sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-      end
-      on_intel do
-        url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-darwin-x86_64.tar.gz"
-        sha256 "ba554488af61165703a20d32df31174daff54056979b4a80504da35a003e4c41"
-      end
-    end
+    arch = Hardware::CPU.arm? ? "aarch64-apple-darwin" : "x86_64-apple-darwin"
 
-    # Download and install macOS binaries
-    ohai "Downloading macOS binaries..."
-    system "curl", "-OL", "#{binary_url}"
-    system "tar", "xzf", "#{binary_filename}"
-    bin.install "vedic" # Install the binary
+    # Install the appropriate rust target
+    system "rustup", "target", "add", arch, "--toolchain", "stable"
 
-    # Cleanup
-    system "rm", "#{binary_filename}"
+    # Build for the specific architecture
+    system "cargo", "build", "--release", "--target", arch
+    bin.install "target/#{arch}/release/vedic"
   end
 
   def caveats
@@ -41,7 +32,6 @@ class Vedic < Formula
   end
 
   test do
-    # Simple test to verify the binary is installed
     assert_match "Usage:", shell_output("#{bin}/vedic --help")
   end
 end
